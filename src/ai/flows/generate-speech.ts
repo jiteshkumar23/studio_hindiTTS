06368cdb54c2bacec2de3748160d9370036c5445
Jitesh Kumar <jitesh23march@gmail.com>
2025-07-27 08:21:08 +0000

    i want this voice ->aoede

--- src/ai/flows/generate-speech.ts
+++ src/ai/flows/generate-speech.ts
@@ -66,7 +66,7 @@ const generateSpeechFlow = ai.defineFlow(
         responseModalities: ['AUDIO'],
         speechConfig: {
           voiceConfig: {
-            prebuiltVoiceConfig: {voiceName: 'en-IN-Wavenet-C'},
+            prebuiltVoiceConfig: {voiceName: 'aoede'},
           },
         },
       },
